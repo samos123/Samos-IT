72288a8fece338dddbb19671bddf27348f6b2602
Sam Stoelinga <sammiestoel@gmail.com>
2011-01-24 16:52:49 +0100

    added projects list to projects pages

--- samosit/projects/views.py
+++ samosit/projects/views.py
@@ -4,9 +4,10 @@ from django.template import RequestContext
 
 
 def index(request):
-    objects = Project.objects.all()
-    return render_to_response('projects/project_index.html', {'objects' : objects}, context_instance=RequestContext(request))
+    projects = Project.objects.all()
+    return render_to_response('projects/project_index.html', {'projects' : projects}, context_instance=RequestContext(request))
 
 def detail(request, project_id, slug):
     project = get_object_or_404(Project, pk=project_id)
-    return render_to_response('projects/project_detail.html', {'object' : project}, context_instance=RequestContext(request))
+    projects = Project.objects.all()
+    return render_to_response('projects/project_detail.html', {'object' : project, 'projects': projects}, context_instance=RequestContext(request))
